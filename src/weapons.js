01f3f8287849fa442c0673ba605cf3c58b48574e
SomeArtem <Serebryakov.artemiy@mail.ru>
2025-05-19 23:01:04 +0300

    190525-2300-weaponlist

---
+++ src/weapons.js
@@ -0,0 +1,462 @@
+export default [
+  {
+    theName:'BudgetArms C-13',
+    type:'P',
+    precesion:-1,
+    damage:'1d6',
+    shots:8,
+    ROF:2,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Dai Lung Cybermag 15',
+    type:'P',
+    precesion:-1,
+    damage:'1d6+1',
+    shots:10,
+    ROF:2,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Federated Arms X-22',
+    type:'P',
+    precesion:0,
+    damage:'1d6+1',
+    shots:10,
+    ROF:2,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Colt .38 Detective',
+    type:'P',
+    precesion:1,
+    damage:'1d6+2',
+    shots:6,
+    ROF:1,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Militech Arms Avenger',
+    type:'P',
+    precesion:0,
+    damage:'2d6+1',
+    shots:10,
+    ROF:2,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Federated Arms X-9',
+    type:'P',
+    precesion:0,
+    damage:'2d6+1',
+    shots:12,
+    ROF:2,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Colt .45 Peacemaker',
+    type:'P',
+    precesion:0,
+    damage:'2d6+2',
+    shots:6,
+    ROF:1,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Dai Lung Streetmaster',
+    type:'P',
+    precesion:0,
+    damage:'2d6+3',
+    shots:12,
+    ROF:2,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'S&W Combat Magnum',
+    type:'P',
+    precesion:1,
+    damage:'2d6+3',
+    shots:6,
+    ROF:2,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'C.O.P. Derringer',
+    type:'P',
+    precesion:0,
+    damage:'2d6+3',
+    shots:4,
+    ROF:2,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'BudgetArms Auto 3',
+    type:'P',
+    precesion:-1,
+    damage:'3d6',
+    shots:8,
+    ROF:2,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Sternmeyer Type 35',
+    type:'P',
+    precesion:0,
+    damage:'3d6',
+    shots:8,
+    ROF:2,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Llama Commanche',
+    type:'P',
+    precesion:0,
+    damage:'4d6',
+    shots:6,
+    ROF:1,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Armalite 44',
+    type:'P',
+    precesion:0,
+    damage:'4d6+1',
+    shots:8,
+    ROF:1,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Colt AMT Model 2000',
+    type:'P',
+    precesion:0,
+    damage:'4d6+1',
+    shots:8,
+    ROF:1,
+    range:50,
+    relatedSkill:'Handgun'
+  },
+  {
+    theName:'Vz61 Skorpion',
+    type:'SMG',
+    precesion:2,
+    damage:'1d6',
+    shots:20,
+    ROF:25,
+    range:150,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'Fed.Arms Tech Assault 2',
+    type:'SMG',
+    precesion:1,
+    damage:'1d6+1',
+    shots:50,
+    ROF:25,
+    range:150,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'Uzi Miniauto 9',
+    type:'SMG',
+    precesion:1,
+    damage:'2d6+1',
+    shots:30,
+    ROF:35,
+    range:150,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'H&K MPK-9',
+    type:'SMG',
+    precesion:1,
+    damage:'2d6+1',
+    shots:35,
+    ROF:25,
+    range:150,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'Uzi',
+    type:'SMG',
+    precesion:2,
+    damage:'2d6+1',
+    shots:30,
+    ROF:20,
+    range:200,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'Ingram MAC 10',
+    type:'SMG',
+    precesion:-1,
+    damage:'2d6+2',
+    shots:30,
+    ROF:5,
+    range:200,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'Arasaka Minami 10',
+    type:'SMG',
+    precesion:0,
+    damage:'2d6+3',
+    shots:40,
+    ROF:20,
+    range:200,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'H&K MP-2013',
+    type:'SMG',
+    precesion:1,
+    damage:'2d6+3',
+    shots:35,
+    ROF:32,
+    range:200,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'Thompson M1',
+    type:'SMG',
+    precesion:2,
+    damage:'2d6+2',
+    shots:30,
+    ROF:20,
+    range:200,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'Bushmaster',
+    type:'SMG',
+    precesion:0,
+    damage:'4d6',
+    shots:30,
+    ROF:20,
+    range:200,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'H&K MPK-11',
+    type:'SMG',
+    precesion:0,
+    damage:'4d6+1',
+    shots:30,
+    ROF:20,
+    range:200,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'Ingram Mac 14',
+    type:'SMG',
+    precesion:-2,
+    damage:'4d6+1',
+    shots:40,
+    ROF:10,
+    range:200,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'Arasaka WCAA Rapid 12',
+    type:'SHT',
+    precesion:-1,
+    damage:'4d6',
+    shots:20,
+    ROF:10,
+    range:50,
+    relatedSkill:'Rifle'
+  },
+  {
+    theName:'Sternmeyer Stakeout 10',
+    type:'SHT',
+    precesion:-2,
+    damage:'4d6',
+    shots:10,
+    ROF:2,
+    range:50,
+    relatedSkill:'Rifle'
+  },
+  {
+    theName:'CAWS',
+    type:'SHT',
+    precesion:0,
+    damage:'4d6',
+    shots:10,
+    ROF:10,
+    range:50,
+    relatedSkill:'Rifle'
+  },
+  {
+    theName:'AKR-20 Medium Assault',
+    type:'RIF',
+    precesion:0,
+    damage:'5d6',
+    shots:30,
+    ROF:30,
+    range:400,
+    relatedSkill:'Rifle'
+  },
+  {
+    theName:'M-16A',
+    type:'RIF',
+    precesion:2,
+    damage:'4d6',
+    shots:30,
+    ROF:25,
+    range:400,
+    relatedSkill:'Rifle'
+  },
+  {
+    theName:'Steyr AUG',
+    type:'RIF',
+    precesion:2,
+    damage:'4d6',
+    shots:30,
+    ROF:20,
+    range:400,
+    relatedSkill:'Rifle'
+  },
+  {
+    theName:'Militech Ronin Light',
+    type:'RIF',
+    precesion:1,
+    damage:'5d6',
+    shots:35,
+    ROF:30,
+    range:400,
+    relatedSkill:'Rifle'
+  },
+  {
+    theName:'AK-47 Medium Assault',
+    type:'RIF',
+    precesion:0,
+    damage:'5d6',
+    shots:30,
+    ROF:20,
+    range:400,
+    relatedSkill:'Rifle'
+  },
+  {
+    theName:'Kalashnikov A-80',
+    type:'RIF',
+    precesion:-1,
+    damage:'6d6+2',
+    shots:35,
+    ROF:25,
+    range:400,
+    relatedSkill:'Rifle'
+  },
+  {
+    theName:'Winchester M70',
+    type:'RIF',
+    precesion:+3,
+    damage:'5d6+1',
+    shots:5,
+    ROF:1,
+    range:500,
+    relatedSkill:'Rifle'
+  },
+  {
+    theName:'KA F-253 Flamethrower',
+    type:'HVY',
+    precesion:-2,
+    damage:'2d10',
+    shots:10,
+    ROF:1,
+    range:50,
+    relatedSkill:'HeavyWeapons'
+  },
+  {
+    theName:'Barrett-Arasaka 20mm Sniper Rifle',
+    type:'HVY',
+    precesion:0,
+    damage:'4d10',
+    shots:10,
+    ROF:1,
+    range:450,
+    relatedSkill:'HeavyWeapons'
+  },
+  {
+    theName:'Commercial 40mm Grenade Launcher',
+    type:'HVY',
+    precesion:0,
+    damage:'22222222',
+    shots:1,
+    ROF:1,
+    range:225,
+    relatedSkill:'HeavyWeapons'
+  },
+  {
+    theName:'Sternmeyer SMG 21',
+    type:'SMG',
+    precesion:0,
+    damage:'3d6',
+    shots:30,
+    ROF:15,
+    range:200,
+    relatedSkill:'Submachinegun'
+  },
+  {
+    theName:'FN-RAL Heavy Assault',
+    type:'RIF',
+    precesion:-1,
+    damage:'6d6+2',
+    shots:30,
+    ROF:30,
+    range:400,
+    relatedSkill:'Rifle'
+  },
+  
+]
+`
+ Name                     Type WA .      Damage   #Shots     ROF   Range 
+ 
+ 
+ Avante Needlegun           P +0 Drugs (Needle) 15 2            40m 
+Enertex Power Squirt       P -2 Drugs 50 1                     10m
+Militech Laser Cannon      RIF +0 N R 1-5d6 10 2                   200m
+Militech Taser             P -1 Stun -2 10 1                   10m
+Nelspot "Wombat"           P -1 Drugs 20 2                     40m 
+Techtron 15 Microwaver     P +0 1d6 + special 10 2             20m 
+ 
+Switchblade                MEL +0 P C 1d6/2* - - - 1m 15 CP20
+Club MEL +0 L C 1d6 - - - 1m 2 CP20
+Knife MEL +0 P C 1d6* - - - Throw 1-20 CP20
+Brass Knuckles Punch +0 P C 1d6+2 - - - 1m 10 CP20
+SPM-1 Battleglove Punch -2 N P 2d6/3d6 - - VR 1m 900 CP20
+Sword MEL +0 N C 2d6+2* - - - 1m 20-200 CP20
+Axe MEL -1 N C 2d6+3* - - - 1m 20 CP20
+Sledgehammer MEL -1 N C 4d6 - - - 1m 20 CP20
+Chainsaw MEL -3 N C 4d6* - - - 2m 80 CP20
+
+Kendachi Monoknife MEL +1 P P 2d6# - - VR 1m 200 CP20
+Nunchaku MEL +0 L C 3d6 - - - 1m 15 CP20
+Naginata MEL +0 N P 3d6* - - - 2m 100 CP20
+Kendachi MonoNaginata MEL +0 N R 4d6# - - VR 2m 400 CP20
+Kendachi MonoKatana MEL +1 N R 4d6# - - VR 1m 600 CP20
+
+ 
+Scratchers Punch 1d6/3* 1m 100 2d6 CP20
+Talon Foot Kick 1d6* 1m 600 2d6 CP20
+Big Knucks Punch 1d6+2 1m 500 3d6 CP20
+Rippers Punch 1d6+3* 1m 400 3d6 CP20
+Spike Hand Punch 1d6+3AP 1m 500 2d6 CP20
+Hammer Hand Punch 1d10 1m 600 2d6 CP20
+Spike Heel Foot Kick 2d6AP 1m 500 2d6 CP20
+Wolvers Punch 3d6* 1m 600 3d6+1 CP20
+Slice N' Dice MEL 2d6# 2m 700 3d6 CP20
+Cybersnake EX 1d6 1m 1200 4d6 CP20
+`
